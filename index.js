e5754e908867e3420e6b16ad3577000b609bdfb8
Ry-F3 <rhysfeew@gmail.com>
2023-04-10 17:14:53 +0000

    Add- learning js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let title = "This is the Title of a Book";
+let lowerTitle = title.toLowerCase();
+console.log(lowerTitle);  // this is the title of a book
+
+
+let difTitle = "This is the Title of a Book";
+let upperTitle = difTitle.toUpperCase();
+console.log(upperTitle);  // this is the title of a book
+
+let string = "Hello"
+
+console.log(typeof string)
+
+
+let myInt = 123;
+let myDecimal = 123.5;
+
+console.log(myInt === myDecimal)
+
+let myNum = 145;
+myNum += 1;
+
+let multiplier = 2;
+
+let product = myNum * multiplier;
+
+console.log(product)
